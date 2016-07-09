4479d08b25121668273ff862fddb947ad35841a5
yupingshang <yuping.shang@gmail.com>
2016-07-09 14:28:56 -0700

    Initial repository

---
+++ src/main/scala/rules/util/Func.scala
@@ -0,0 +1,11 @@
+package rules.util
+
+object Func {
+
+  implicit def toListHelper[A](l: List[A]) = new {
+    def +?[B <: A] (item: Option[B]) = item match {
+      case Some(b) => b :: l
+      case None => l
+    }
+  }
+}
